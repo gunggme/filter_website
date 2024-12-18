18441d2eca806786b164890ff43cbf1d7ac9f89d
gunggme <ekfrifajfl2019@gmail.com>
2024-12-18 15:20:52 +0900

    테스트중?

--- src/types/images.d.ts
+++ src/types/images.d.ts
@@ -6,4 +6,9 @@ declare module '*.jpg' {
 declare module '*.png' {
   const value: string
   export default value
+}
+
+// 이미지 인덱스 시그니처 타입 추가
+interface ImageImports {
+  [key: number]: () => Promise<typeof import('*.png')>
 } 
